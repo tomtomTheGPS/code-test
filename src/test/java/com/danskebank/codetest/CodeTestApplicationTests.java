8325a27b9cc640cc619110905cff4d2f2d9dcaed
Thomas <tmyrup@gmail.com>
2023-06-14 08:47:38 +0200

    Initial setup

---
+++ src/test/java/com/danskebank/codetest/CodeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danskebank.codetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
