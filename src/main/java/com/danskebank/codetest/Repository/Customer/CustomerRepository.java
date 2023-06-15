3c2d32953233e63b893bb17e7c635f0bb2d4624d
Thomas <tmyrup@gmail.com>
2023-06-15 12:49:29 +0200

    Updated CustomerRepository

--- src/main/java/com/danskebank/codetest/Repository/Customer/CustomerRepository.java
+++ src/main/java/com/danskebank/codetest/Repository/Customer/CustomerRepository.java
@@ -1,16 +1,24 @@
 package com.danskebank.codetest.Repository.Customer;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import com.danskebank.codetest.Model.Account.Account;
 import com.danskebank.codetest.Model.Customer.Customer;
+import com.danskebank.codetest.Repository.BaseRepository;
 
 public class CustomerRepository {
 
     private Map<Long, Customer> customers = new HashMap<>();
+    private BaseRepository baseRepository;
 
-    public void addCustomer(Customer customer) {
-        customers.put(customer.getID(), customer);
+    public void addCustomer(String name) {
+        List<Account> accounts = new ArrayList<>();
+        long id = baseRepository.generateID();
+        Customer customer = new Customer(id, name, accounts);
+        customers.put(id, customer);
     }
 
     public Customer getCustomer(long id) {
